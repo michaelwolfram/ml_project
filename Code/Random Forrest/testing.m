ee986ecb0bac604b3e21af8738688c6ef903957a
Michael Wolfram <michaelwolfram@gmx.de>
2014-01-05 00:37:39 +0100

    Commit for this day. Test is running over night.

--- Code/Random Forrest/testing.m
+++ Code/Random Forrest/testing.m
@@ -1,16 +1,91 @@
-bT = BinaryTree('gini','p',true,'d',5);
-bT = bT.trainImpurityMeasure(train_data,train_labels);
+maxDepth = 10;
 
-% numDataPoints = 10;
-numDataPoints = size(valid_data,1);
+accuracies = zeros(3,maxDepth);
 
+startAccuraciesString = 'Evaluation/Accuracies/accuracy';
+matEnd = '.mat';
 
-correct = 0;
-for i=1:numDataPoints
-    classification = bT.classify(valid_data(i,:));
-    if strcmp(classification,valid_labels{i})
-        correct = correct + 1;
+try
+    for i = 1:maxDepth
+        maxHeight = i;
+        
+        bT = BinaryTree('gini','p',true,'d',maxHeight);
+        bT = bT.trainImpurityMeasure(train_data,train_labels);
+        
+        numDataPoints = size(valid_data,1);
+        correct = 0;
+        for j=1:numDataPoints
+            classification = bT.classify(valid_data(j,:));
+            if strcmp(classification,valid_labels{j})
+                correct = correct + 1;
+            end
+        end
+        accuracy = correct/numDataPoints;
+        accuracies(1,i) = accuracy;
+        
+        saveString = strcat(startAccuraciesString,'_',num2str(i), ...
+            '_gini',matEnd);
+        save(saveString,'accuracy','-v7.3');
+        
+        
+        %%%%%
+        clear bT;
+        bT = BinaryTree('entropy','p',true,'d',maxHeight);
+        bT = bT.trainImpurityMeasure(train_data,train_labels);
+        
+        numDataPoints = size(valid_data,1);
+        correct = 0;
+        for j=1:numDataPoints
+            classification = bT.classify(valid_data(j,:));
+            if strcmp(classification,valid_labels{j})
+                correct = correct + 1;
+            end
+        end
+        accuracy = correct/numDataPoints;
+        accuracies(2,i) = accuracy;
+        
+        saveString = strcat(startAccuraciesString,'_',num2str(i), ...
+            '_entropy',matEnd);
+        save(saveString,'accuracy','-v7.3');
+        
+        
+        %%%%%
+        clear bT;
+        bT = BinaryTree('misclassRate','p',true,'d',maxHeight);
+        bT = bT.trainImpurityMeasure(train_data,train_labels);
+        
+        numDataPoints = size(valid_data,1);
+        correct = 0;
+        for j=1:numDataPoints
+            classification = bT.classify(valid_data(j,:));
+            if strcmp(classification,valid_labels{j})
+                correct = correct + 1;
+            end
+        end
+        accuracy = correct/numDataPoints;
+        accuracies(3,i) = accuracy;
+        
+        saveString = strcat(startAccuraciesString,'_',num2str(i), ...
+            '_misclassRate',matEnd);
+        save(saveString,'accuracy','-v7.3');
+        
+        
+        %%%%%
+        saveString = strcat(startAccuraciesString,'_',num2str(i), ...
+            '_all',matEnd);
+        save(saveString,'accuracies','-v7.3');
     end
+catch
+    saveString = strcat(startAccuraciesString,'_',num2str(i), ...
+        '_all_catch',matEnd);
+    save(saveString,'accuracies','-v7.3');
 end
 
-correct/numDataPoints
+saveString = strcat(startAccuraciesString,'_',num2str(i), ...
+    '_all_final',matEnd);
+save(saveString,'accuracies','-v7.3');
+
+
+
+
+
