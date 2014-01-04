a0ffc7021d33580b1494465d71d17bdc53b38aeb
Jonas Uhrig <jonas.uhrig@gmx.de>
2014-01-04 17:52:06 +0100

    minor testing..

--- Code/Ferns/Random Ferns/testScript.m
+++ Code/Ferns/Random Ferns/testScript.m
@@ -13,27 +13,27 @@
 
 %% Ferest Tests
 
-% numFerns = 10;
-% numTests = 10;
-% ferest = Ferest(numFerns, numTests);
-% tic
-% ferest = ferest.trainRandom(train_data, train_labels);
-% display('Elapsed time for the training of the FEREST:')
-% toc
+numFerns = 10;
+numTests = 10;
+ferest = Ferest(numFerns, numTests);
+tic
+ferest = ferest.trainRandom(train_data, train_labels);
+display('Elapsed time for the training of the FEREST:')
+toc
 
 
 % left entry for correct, right for wrong :)
-accuracy = [0, 0];
-tic
-for i = 1:size(train_data,1)
-    sample = train_data(i,:);
-    [class, posterior] = ferest.evaluate(sample);
-    if strcmp(class,train_labels{i}) == 1
-        accuracy(1) = accuracy(1)+1;
-    else
-        accuracy(2) = accuracy(2)+1;
-    end
-end
-display('Elapsed time for evaluation of the training set:');
-toc
-accuracy = accuracy/sum(accuracy)
+% accuracy = [0, 0];
+% tic
+% for i = 1:size(train_data,1)
+%     sample = train_data(i,:);
+%     [class, posterior] = ferest.evaluate(sample);
+%     if strcmp(class,train_labels{i}) == 1
+%         accuracy(1) = accuracy(1)+1;
+%     else
+%         accuracy(2) = accuracy(2)+1;
+%     end
+% end
+% display('Elapsed time for evaluation of the training set:');
+% toc
+% accuracy = accuracy/sum(accuracy)
