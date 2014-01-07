4124e38e4405a0e9353e7b7fdb14f4ed8e041864
Jonas Uhrig <jonas.uhrig@gmx.de>
2014-01-07 21:15:08 +0100

    forest on all data from depth 6 with size 1 to 14 done. Glumanda

--- Code/Random Forrest/Evaluation_Forests/Gini_only_All/evaluateForestAllDataSeveralForests.m
+++ Code/Random Forrest/Evaluation_Forests/Gini_only_All/evaluateForestAllDataSeveralForests.m
@@ -10,190 +10,190 @@ impurityMeasure = 'gini';
 accuracies = zeros(18,1);
 testTimes = zeros(18,1);
 
-startAccuraciesString = 'Evaluation_Forests/Gini_only_All/accuracies';
+startAccuraciesString = 'accuracies';
 matEnd = '.mat';
 
 numDataPoints = size(valid_data,1);
 
-%% 1
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_1.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
-
-accuracies(1) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(1) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(1),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
-
-%% 2
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_2.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
-
-accuracies(2) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(2) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(2),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
-
-%% 3
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_3.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
-
-accuracies(3) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(3) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(3),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
-
-%% 4
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_4.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
-
-accuracies(4) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(4) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(4),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
-
-%% 5
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_5.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
-
-accuracies(5) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(5) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(5),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
-
-%% 6
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_6.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
-
-accuracies(6) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(6) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(6),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
-
-%% 7
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_7.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
-
-accuracies(7) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(7) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(7),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
-
-%% 8
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_8.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
-
-accuracies(8) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(8) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(8),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
-
-%% 9
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_9.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
-
-accuracies(9) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(9) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(9),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
+% %% 1
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_1.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(1) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(1) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(1),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
+% 
+% %% 2
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_2.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(2) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(2) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(2),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
+% 
+% %% 3
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_3.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(3) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(3) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(3),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
+% 
+% %% 4
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_4.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(4) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(4) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(4),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
+% 
+% %% 5
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_5.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(5) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(5) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(5),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
+% 
+% %% 6
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_6.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(6) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(6) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(6),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
+% 
+% %% 7
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_7.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(7) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(7) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(7),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
+% 
+% %% 8
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_8.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(8) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(8) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(8),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
+% 
+% %% 9
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_9.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(9) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(9) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(9),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
 
 %% 10
 % Classify testing data.
@@ -295,87 +295,88 @@ saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
     '_',num2str(6),'_',num2str(14),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
 save(saveString,'accuracy','Ttest','-v7.3');
 
-%% 15
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_15.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
+% %% 15
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_15.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(15) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(15) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(15),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
+% 
+% %% 16
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_16.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(16) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(16) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(16),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
+% 
+% %% 17
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_17.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(17) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(17) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(17),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
+% 
+% %% 18
+% % Classify testing data.
+% correct = 0;
+% tic
+% for j=1:numDataPoints
+%     classification = forest_18.classify(valid_data(j,:));%%%%%%%%%%%%%%%
+%     if strcmp(classification,valid_labels{j})
+%         correct = correct + 1;
+%     end
+% end
+% Ttest=toc
+% accuracy = correct/numDataPoints;
+% 
+% accuracies(18) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% testTimes(18) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
+%     '_',num2str(6),'_',num2str(18),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
+% save(saveString,'accuracy','Ttest','-v7.3');
+% 
 
-accuracies(15) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(15) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(15),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
-
-%% 16
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_16.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
-
-accuracies(16) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(16) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(16),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
-
-%% 17
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_17.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
-
-accuracies(17) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(17) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(17),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
-
-%% 18
-% Classify testing data.
-correct = 0;
-tic
-for j=1:numDataPoints
-    classification = forest_18.classify(valid_data(j,:));%%%%%%%%%%%%%%%
-    if strcmp(classification,valid_labels{j})
-        correct = correct + 1;
-    end
-end
-Ttest=toc
-accuracy = correct/numDataPoints;
-
-accuracies(18) = accuracy;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-testTimes(18) = Ttest;%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-saveString = strcat(startAccuraciesString,'_',impurityMeasure,...
-    '_',num2str(6),'_',num2str(18),'_',matEnd);%%%%%%%%%%%%%%%%%%%%%%%%%
-save(saveString,'accuracy','Ttest','-v7.3');
-
-
+%%
 saveString = strcat(startAccuraciesString,'_',impurityMeasure, ...
     '_',num2str(6),'_all_final',matEnd);
 save(saveString,'accuracies','testTimes','-v7.3');
